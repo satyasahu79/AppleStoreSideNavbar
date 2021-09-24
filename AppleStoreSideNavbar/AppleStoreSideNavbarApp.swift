6775f72c2f15954e779216ad54f03addfd24ab72
Satya Prakash Sahu <satyasahu79@gmail.com>
2021-09-24 19:03:02 +0530

    Initial Commit

---
+++ AppleStoreSideNavbar/AppleStoreSideNavbarApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleStoreSideNavbarApp.swift
+//  AppleStoreSideNavbar
+//
+//  Created by Satya Prakash Sahu on 24/09/21.
+//
+
+import SwiftUI
+
+@main
+struct AppleStoreSideNavbarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
